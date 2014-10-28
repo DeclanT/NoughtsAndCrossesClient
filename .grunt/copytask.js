5f7889fc94d832e8d5b431b7895d237873a4378a
DeclanT <declan.taylor@tombola-0270.local>
2014-10-28 10:42:14 +0000

    updated gruntfile.js

---
+++ .grunt/copytask.js
@@ -0,0 +1,3 @@
+/**
+ * Created by declan.taylor on 28/10/2014.
+ */
--- gruntfile.js
+++ gruntfile.js
@@ -1,12 +1,19 @@
-/**
- * Created by declan.taylor on 28/10/2014.
- */
+module.exports = function(grunt) {
 
-grunt.loadNpmTasks('grunt-contrib-jshint');
-grunt.loadNpmTasks('grunt-lesslint');
-grunt.loadNpmTasks('grunt-contrib-less');
-grunt.loadNpmTasks('grunt-contrib-clean');
-grunt.loadNpmTasks('grunt-include-replace');
-grunt.loadNpmTasks('grunt-contrib-copy');
-grunt.loadNpmTasks('grunt-express-server');
-grunt.loadNpmTasks('grunt-contrib-watch');
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-lesslint');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-include-replace');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-express-server');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+    grunt.registerTask('default',[]);
+};
