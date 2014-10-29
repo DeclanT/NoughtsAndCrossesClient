a56951dfc68cf5416628b8de3777f5e8a7c6134a
SalamanderMan <andrew.welsh@tombola-498.local>
2014-10-29 09:46:29 +0000

    Less lint now works, CSS gets tidied up

--- .grunt/lesstask.js
+++ .grunt/lesstask.js
@@ -1,3 +1,13 @@
 /**
  * Created by andrew.welsh on 28/10/2014.
  */
+module.exports =  {
+
+    css: {
+        cwd: "",
+        src: "main-app/app/CSS/*",
+        dest: ".build/",
+        expand: true,
+    }
+
+};
