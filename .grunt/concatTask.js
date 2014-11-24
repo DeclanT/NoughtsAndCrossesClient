2910ad286425f4eaa229749f9ca8d4d7470542a7
DeclanT <declan.taylor@tombola-0270.local>
2014-11-24 12:44:14 +0000

    game board buttons removed, onclick added to table

--- .grunt/concatTask.js
+++ .grunt/concatTask.js
@@ -1,6 +1,6 @@
 module.exports =  {
             scripts: {
-                src: ['main-app/app/scripts/servercomms.js','main-app/app/scripts/*.js'],
+                src: ['main-app/app/scripts/controller.js','main-app/app/scripts/*.js'],
                 dest: '.build/main-app/app/scripts/built.js',
             },
 
--- .grunt/copytask.js
+++ .grunt/copytask.js
@@ -5,6 +5,20 @@ module.exports =  {
             dest: '.build/',
             expand:true,
         },
+
+        html:{
+            cwd: '',
+            src: 'main-app/app/index.html',
+            dest: '.build/',
+            expand:true,
+        },
+
+        angular:{
+            cwd:'',
+            src:'angular/*',
+            dest: '.build/main-app/app',
+            expand:true,
+        },
 };
 
 
