639425eaab05b47f82a8757b64d1becf0e1fb7fb
DeclanT <declan.taylor@tombola-0270.local>
2014-11-27 15:18:12 +0000

    initial model commit and tidy up

--- main-app/app/scripts/controllers.js
+++ main-app/app/scripts/controllers.js
@@ -1,3 +1,7 @@
-noughtsAndCrossesApp.controller('noughtsAndCrossesController', function ($scope){
+noughtsAndCrossesApp.controller('noughtsAndCrossesController', function ($scope,gameModel){
 
+    $scope.gameModel = gameModel;
+    $scope.displayoutcome=function(){
+        console.log($scope.gameModel.gameBoard);
+    };
 });
---
+++ main-app/app/scripts/gamemodel.js
@@ -0,0 +1,7 @@
+noughtsAndCrossesApp.factory('gameModel',function() {
+    return {
+        outcome: 'continue',
+        gameboard: '000000000',
+        winner: 0
+    };
+});
