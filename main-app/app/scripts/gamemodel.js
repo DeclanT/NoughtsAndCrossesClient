63f162abf6747fc21e461cefcc248fb27f589d23
DeclanT <declan.taylor@tombola-0270.local>
2014-12-01 10:34:11 +0000

    extracting from the controller

--- main-app/app/scripts/gamemodel.js
+++ main-app/app/scripts/gamemodel.js
@@ -1,9 +1,42 @@
-noughtsAndCrossesApp.factory('gameModel',function() {
-    return {
-        outcome: 'Continue',
-        gameboard: '000000000',
-        winner: 0,
+noughtsAndCrossesApp.factory('gameModel',function($http) {
+
+
+    var model = function () {
+        outcome = 'Continue';
+        gameboard = '000000000';
+        winner = 0;
+
+        serverPost = {
+            method: 'POST',
+            url: '',
+            'withCredentials': 'true',
+            headers: {
+                'content-type': 'application/json'
+            },
+            data: '',
+        };
+
+        newGame = function () {
+            serverPost.url = 'http://tictactoe1.cloudapp.net:35000/api/v1.0/newgame';
+            serverPost.data = {'player1':'random', 'player2':'human'};
+            $http(serverPost).
+                success(function (data) {
+                    model.outcome = data.outcome;
+                    model.gameboard = data.gameboard;
+                    model.winner = data.winner;
+                });
+        };
+
+        makeMove = function (squareNumber) {
+            serverPost.url = 'http://tictactoe1.cloudapp.net:35000/api/v1.0/makemove';
+            serverPost.data = {'playerNumber': 2, 'chosenSquare': squareNumber};
+            $http(serverPost).
+                success(function (data) {
+                    model.outcome = data.outcome;
+                    model.gameboard = data.gameboard;
+                    model.winner = data.winner;
+                });
+        };
     };
-
-
+    return model;
 });
