9555d14015507c5cad26bf5c9dfcd3b0830d84d0
DeclanT <declan.taylor@tombola-0270.local>
2014-12-04 13:09:03 +0000

    handles the winner outcome with a placeholder of an alert.

---
+++ main-app/app/scripts/services/handleoutcome-service.js
@@ -0,0 +1,19 @@
+noughtsAndCrossesApp.service('handleOutcome',function(gameModel){
+
+    this.handle= function(){
+        if (gameModel.outcome === 'Win') {
+            handleWin();
+        }
+        else if (gameModel.outcome === 'Draw') {
+            handleDraw();
+        }
+    };
+
+    var handleWin = function(){
+            alert('Player'+gameModel.winner+ ' has WON!');
+    };
+
+    var handleDraw = function(){
+        alert('game was a draw');
+    };
+});
