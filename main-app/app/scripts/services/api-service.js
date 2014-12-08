4f7d54a39f12e57611d532683af82cbf1151b9bb
DeclanT <declan.taylor@tombola-0270.local>
2014-12-08 12:00:45 +0000

    removed unneeded variables

--- main-app/app/scripts/services/api-service.js
+++ main-app/app/scripts/services/api-service.js
@@ -2,15 +2,11 @@ angular.module('noughtsAndCrossesApp').
     service('apiService',['$http','gameModel','updateModel','handleOutcome',function($http,gameModel,updateModel,handleOutcome) {
 
     this.makeMove = function (squareNumber) {
-        var url ='http://tictactoe1.cloudapp.net:35000/api/v1.0/makemove';
-        var data= {'playerNumber': gameModel.currentPlayer, 'chosenSquare': squareNumber};
-        makeCall(url,data);
+        makeCall('http://tictactoe1.cloudapp.net:35000/api/v1.0/makemove',{'playerNumber': gameModel.currentPlayer, 'chosenSquare': squareNumber});
     };
 
     this.newGame = function(){
-        var url='http://tictactoe1.cloudapp.net:35000/api/v1.0/newgame';
-        var data={'player1':gameModel.player1,'player2':gameModel.player2};
-        makeCall(url,data);
+        makeCall('http://tictactoe1.cloudapp.net:35000/api/v1.0/newgame',{'player1':gameModel.player1,'player2':gameModel.player2});
     };
 
     var makeCall = function(url,data) {
