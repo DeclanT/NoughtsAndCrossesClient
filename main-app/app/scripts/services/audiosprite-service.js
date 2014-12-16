2d960c181c487f9e8084b4d5c789ace8b9724496
DeclanT <declan.taylor@tombola-0270.local>
2014-12-16 14:38:09 +0000

    added variable for the sound src

--- main-app/app/scripts/services/audiosprite-service.js
+++ main-app/app/scripts/services/audiosprite-service.js
@@ -4,20 +4,21 @@
         factory('audioService', ['$document', function ($document) {
 
            var AudioSprite = function() {
-               var audioNode = angular.element('<audio id="soundSprite">');
 
+               var clicksound = 'sounds/click.mp3';
+
+               var audioNode = angular.element('<audio id="soundSprite">');
 
                var createSourceNode = function() {
                    var sourceNode = angular.element('<source>');
-
-                   sourceNode.attr('src','sounds/click.mp3');
+                   sourceNode.attr('src',clicksound);
                    sourceNode.attr('type','audio/mpeg');
 
                    return sourceNode;
                };
 
                audioNode.attr('preload', 'auto');
-               audioNode.attr('src', 'sounds/click.mp3');
+               audioNode.attr('src', clicksound);
                audioNode.append(createSourceNode());
 
 
